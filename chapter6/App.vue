3baa17b8df8adbfb520a06b25f619dcf4aaab353
ryo <3955027+older4@users.noreply.github.com>
2020-11-13 22:27:44 +0900

    add chapter 6

---
+++ chapter6/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="message">メッセージ: {{ msg }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return { msg: "こんちは" };
+  },
+};
+</script>
+
+<style>
+.message {
+  color: aqua;
+}
+</style>
